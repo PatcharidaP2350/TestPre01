4e0b9e77abc58ece485513c16b283c9b62397bbd
PatcharidaP2350 <pimridaaa28@gmail.com>
2025-05-22 18:55:02 +0700

    set up

--- backend/entity/activity_factor.go
+++ backend/entity/activity_factor.go
@@ -8,7 +8,7 @@ import (
 
 type ActivityFactor struct {
 	gorm.Model
-	ActivityLevel string     //ระดับการออกกำลังกาย เช่น "กิจกรรมเบา", "สูงมาก"
+	ActivityLevel string  //ระดับการออกกำลังกาย เช่น "กิจกรรมเบา", "สูงมาก"
 	Duration int     //ช่วงเวลาที่ออกกำลังกายในแต่ละวัน (เช่น 15-30 นาที)
 	Frequency int     //ความถี่ต่อสัปดาห์ (เช่น 3-4 ครั้ง/สัปดาห์)
 	EstimatedCalories int     //ปริมาณแคลอรี่ที่คาดว่าจะออกกำลังกาย, ค่าพลังงานที่ใช้ประมาณต่อวัน (kcal)
---
+++ backend/go.mod
@@ -0,0 +1,11 @@
+module backend
+
+go 1.22.5
+
+require gorm.io/gorm v1.26.1
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
